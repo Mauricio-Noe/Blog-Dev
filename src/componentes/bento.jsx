eb5477bf97f55615316f5285ff54fd55cab0ee30
Noe <gnoe8799@gmail.com>
2024-11-05 23:04:17 -0600

    media query bento

--- src/componentes/bento.jsx
+++ src/componentes/bento.jsx
@@ -12,11 +12,11 @@ export const Bentonoti = () => {
       <div className=" bento">Bento 1</div>
       <div className=" bento">Bento 1</div>
       </div>
-      <div className="bento-left">
+      <div className="bento-1">
       <div className=" bento">Bento 1</div>
       <div className=" bento">Bento 1</div>
       </div>
-      <div className="bento-rigt">      
+      <div className="bento-1">      
         <div className=" bento">Bento 3</div>
       </div>
     </div>
